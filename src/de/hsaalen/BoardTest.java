3f5ce5a63673b78e6537a32fe3f95cff8e399b89
alexander.schweiss.2001 <alexander.schweiss.2001@gmail.com>
2024-12-09 16:51:08 +0100

    2a, add test for test_maximum_tile_index_x and test_maximum_tile_index_y in class BoardTest

--- src/de/hsaalen/BoardTest.java
+++ src/de/hsaalen/BoardTest.java
@@ -3,7 +3,23 @@ package de.hsaalen;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.beans.Transient;
+
 public class BoardTest {
+
+    @Test
+    public void test_maximum_tile_index_x(){
+        Board board = new Board();
+        int maximum_tile_index_x = board.maximum_tile_index_x();
+		assertEquals( ( maximum_tile_index_x + 1 ) * board.tile_size_in_pixels, board.width_in_pixels );
+    }
+
+    @Test
+	public void  test_maximum_tile_index_y(){
+		Board board = new Board();
+		int maximum_tile_index_y = board.maximum_tile_index_y();
+		assertEquals( ( maximum_tile_index_y + 1 ) * board.tile_size_in_pixels, board.width_in_pixels );
+	}
 
     @Test
     public void testConcatenate() {
